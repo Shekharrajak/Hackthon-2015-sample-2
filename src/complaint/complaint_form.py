63d97af0e8852e93c29320167e218c83d2d89969
shekharrajak <shekharstudy@ymail.com>
2015-08-23 02:31:13 +0530

    screenshot

--- src/complaint/complaint_form.py
+++ src/complaint/complaint_form.py
@@ -2,7 +2,7 @@ from django import forms
 
 from .models import complaint
 
-class ComplaintForm(complaint_form.ModelForm):
+class ComplaintForm(forms.ModelForm):
 	class Meta:
 		model = complaint
 		fields = ['name', 'complaint_type', 'complaint_desc','address','cit','email']
